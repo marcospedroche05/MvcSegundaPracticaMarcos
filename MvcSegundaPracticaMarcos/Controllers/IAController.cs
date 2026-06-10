40594ae1f145d752cf7a6ea12285982afbe9e5e3
marcospedroche05 <marcospedroche62@gmail.com>
2026-06-10 12:52:21 +0200

    implementacion IA

---
+++ MvcSegundaPracticaMarcos/Controllers/IAController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcSegundaPracticaMarcos.Services;
+
+namespace MvcSegundaPracticaMarcos.Controllers
+{
+    public class IAController : Controller
+    {
+        private ServiceIA service;
+
+        public IAController(ServiceIA service)
+        {
+            this.service = service;
+        }
+
+        // GET /IA
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST /IA
+        [HttpPost]
+        public async Task<IActionResult> Index(string pregunta)
+        {
+            if (!string.IsNullOrEmpty(pregunta))
+            {
+                string respuesta = await this.service.PreguntarAsync(pregunta);
+                ViewBag.Pregunta = pregunta;
+                ViewBag.Respuesta = respuesta;
+            }
+            return View();
+        }
+    }
+}
---
+++ MvcSegundaPracticaMarcos/Services/ServiceIA.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcSegundaPracticaMarcos.Services
+{
+    public class ServiceIA
+    {
+        private string ApiUrl;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceIA(IConfiguration configuration)
+        {
+            this.ApiUrl = configuration.GetValue<string>("ApiUrls:ApiPreguntas");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<string> PreguntarAsync(string pregunta)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+
+                var body = JsonConvert.SerializeObject(new { pregunta = pregunta });
+                StringContent content = new StringContent(body, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(this.ApiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject<dynamic>(json);
+                    return result.respuesta;
+                }
+                else
+                {
+                    return "No se pudo obtener respuesta de la IA.";
+                }
+            }
+        }
+    }
+}
