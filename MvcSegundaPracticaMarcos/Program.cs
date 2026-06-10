12bce73b68118f5756b7188a485d9dbffde7a3b8
marcospedroche05 <marcospedroche62@gmail.com>
2026-06-10 12:54:04 +0200

    inyeccion service

--- MvcSegundaPracticaMarcos/Program.cs
+++ MvcSegundaPracticaMarcos/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<ServiceEventos>();
+builder.Services.AddTransient<ServiceIA>();
 
 var app = builder.Build();
 
